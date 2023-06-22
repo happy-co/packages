1e0b9d28c73e767182f9f41f8e278403eae32034
Marco Savaglia <marco.savaglia@happy.co>
2023-06-22 12:33:56 +0930

    Continuing work on aspect ratio and capture mode

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -114,6 +114,11 @@ class Camera
 
   private final SurfaceTextureEntry flutterTexture;
   private final ResolutionPreset resolutionPreset;
+
+  private final CaptureMode captureMode;
+
+  private final AspectRatioPreset aspectRatioPreset;
+
   private final boolean enableAudio;
   private final Context applicationContext;
   final DartMessenger dartMessenger;
